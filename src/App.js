912d955e2edfb22fdd83c88504eca8f8f2289bff
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-04-17 02:14:11 +0200

    Edited the name of the calendar folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import WeatherWidget from "./components/weatherWidget";
-import Calendar from "./components/Calendar";
+import Calendar from "./components/calendar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
