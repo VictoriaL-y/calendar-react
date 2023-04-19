bfa7ec6eb00945a3b77027dd4c1c955865cc8360
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-04-19 23:46:51 +0200

    Edited an import

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import WeatherWidget from "./components/weatherWidget";
 import Calendar from "./components/calendar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const App = () => {
 
