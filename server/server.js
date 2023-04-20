b2d54b02148db34a1bed22474ff20a6c10c8ec49
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-04-20 03:41:00 +0200

    Edited a link

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'http://localhost:5000',
       changeOrigin: true,
     })
   );
