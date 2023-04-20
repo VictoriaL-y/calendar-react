ddc0c68176cf924f3798636a98c3b17e920d2fea
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-04-20 03:49:50 +0200

    Edited cors

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ module.exports = function(app) {
 };
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "calendar-react-victorial-y.netlify.app"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
