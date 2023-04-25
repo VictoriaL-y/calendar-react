277a849e2e11fe7900c04e957b54500085f094ba
Victoria Lipnitsky <victoria.lipnitsky@gmail.com>
2023-04-25 19:34:06 +0200

    Edited mobile responsivity

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import WeatherWidget from "./components/weatherWidget";
 import Calendar from "./components/calendar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const App = () => {
+  const [windowHeight, setWindowHight] = useState(window.innerHeight);
+  document.documentElement.style.setProperty('--vh', `${windowHeight}px`);
+
+  useEffect(() => {
+    const handleWindowHeight = () => {
+      setWindowHight(window.innerHeight);
+      document.documentElement.style.setProperty('--vh', `${windowHeight}px`);
+    }
+    window.addEventListener('resize', handleWindowHeight);
+
+    return() => {
+      window.removeEventListener('resize', handleWindowHeight);
+    }
+  }, [])
 
   //First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  const vh = window.innerHeight * 0.01;
-  console.log(vh);
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  console.log("my height is " + vh);
+  // const vh = window.innerHeight * 0.01;
+  // console.log(vh);
+  // // Then we set the value in the --vh custom property to the root of the document
+  // document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // console.log("my height is " + vh);
 
   return (
     <>
